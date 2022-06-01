13fd7a6af7c99eae1efab51a29b253f5789a73df
tajwarsultan <tajwar.sultan00@gmail.com>
2022-06-01 12:43:45 +0500

    dashboard is created

--- src/components/view/UseCases/AnalyticsDashboard.jsx
+++ src/components/view/UseCases/AnalyticsDashboard.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import graph from "../../../assets/graphs.png";
+import Efficiency from "../../../assets/Efficiency.svg";
+import Error from "../../../assets/Error.svg";
+import Productivity from "../../../assets/Productivity.svg";
 
 function AnalyticsDashboard() {
+  const items = [
+    { url: Productivity, name: "Improve", btn1: "Productivity" },
+    { url: Efficiency, name: "Improve", btn1: " Efficiency" },
+    {
+      url: Error,
+      name: "Reduce",
+      btn: "Error Rates",
+      and: "and",
+      btn2: "Accidents",
+    },
+  ];
+  const itemsData = items.map((item, i) => (
+    <div key={i} className="cards">
+      <div className="icon">
+        <img src={item.url} alt="icons" />
+      </div>
+      <div className="content">
+        <h4>
+          {item.name} <span>{item.btn}</span>
+          <br /> <span>{item.btn1}</span> {item.and} <span>{item.btn2}</span>
+        </h4>
+      </div>
+    </div>
+  ));
   return (
     <div className="Analytics-Dashboard">
       <div className="analytic-wrapper">
@@ -16,17 +44,10 @@ function AnalyticsDashboard() {
               paths.
             </p>
           </div>
-          <div className="icon-cards">
-              <div className="icon">
-                  <img src="#" alt="icons" />
-              </div>
-              <div className="content">
-                  <h4>Improve Productivity</h4>
-              </div>
-          </div>
+          <div className="icon-cards">{itemsData}</div>
         </div>
         <div className="analytic-picture">
-            <img src="#" alt="graph pictures" />
+          <img src={graph} alt="graph pictures" />
         </div>
       </div>
     </div>
