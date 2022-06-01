bc2d2a7e6eb6179625f74506a824ab7e085f6e4e
tajwarsultan <tajwar.sultan00@gmail.com>
2022-06-01 18:52:20 +0500

    slider is added the recruitment process is moving vertically

--- src/components/view/Career/Recruitment.js
+++ src/components/view/Career/Recruitment.js
@@ -1,6 +1,58 @@
 import React from "react";
+import Slider from "react-slick";
 
 const Recruitment = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    vertical: true,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    verticalSwiping: true,
+    beforeChange: function (currentSlide, nextSlide) {
+      console.log("before change", currentSlide, nextSlide);
+    },
+    afterChange: function (currentSlide) {
+      console.log("after change", currentSlide);
+    },
+  };
+  const items = [
+    {
+      title: "Round 1",
+      content: "Application Review",
+      url: "/images/career/pink.svg",
+    },
+    {
+      title: "Round 2",
+      content: "Application Review",
+      url: "/images/career/pink.svg",
+    },
+    {
+      title: "Round 3",
+      content: "Application Review",
+      url: "/images/career/pink.svg",
+    },
+    {
+      title: "Round 4",
+      content: "Application Review",
+      url: "/images/career/pink.svg",
+    },
+  ];
+  const itemsData = items.map((item, i) => (
+    <div className="Slider">
+      <div className="gray-box">
+        <h5 className="H5 pb-4">{item.title}</h5>
+        <h3 className="m-0 H3 text-white">{item.content}</h3>
+      </div>
+      <div className="w-75 mx-auto pink-box">
+        <img src={item.url} alt="" className="img-fluid " />
+      </div>
+    </div>
+  ));
   return (
     <section className="dark-bg row req-demo-sec mx-0">
       <div className="col-lg-4 col-md-6 my-auto">
@@ -9,13 +61,7 @@ const Recruitment = () => {
         </h2>
       </div>
       <div className="col-lg-4 col-md-6 text-center">
-        <div className="gray-box">
-          <h5 className="H5 pb-4">Round 1</h5>
-          <h3 className="m-0 H3 text-white">Application Review</h3>
-        </div>
-        <div className="w-75 mx-auto pink-box">
-          <img src="/images/career/pink.svg" alt="" className="img-fluid " />
-        </div>
+        <Slider {...settings}>{itemsData}</Slider>
       </div>
     </section>
   );
